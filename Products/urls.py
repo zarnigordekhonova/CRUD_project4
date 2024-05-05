19d886012509dfc956e863985847a4dc79db3f36
zarnigordekhonova <zarnigor1008@gmail.com>
2024-05-05 05:27:46 +0500

    first commit

--- Products/urls.py
+++ Products/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import get_info, get_products
+from .views import get_info, get_products, detail, add_products, update_products, delete_products
 
 
-
+app_name = 'Products'
 urlpatterns = [
     path('', get_info, name='get_info'),
-    path('products/<int:pk>', get_products, name='get_products')
+    path('products/<int:pk>', get_products, name='get_products'),
+    path('products/detail/<int:pk>', detail, name='detail'),
+    path('add_products', add_products, name='add_products'),
+    path('update/<int:pk>', update_products, name='update_products'),
+    path('delete/<int:pk>', delete_products, name='delete_products')
 ]
--- Products/views.py
+++ Products/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Category, Products
+from .forms import ProductsForm
 
 # Create your views here.
 
@@ -17,10 +18,44 @@ def get_products(request, pk):
     }
     return render(request, 'products.html', context=context)
 
-# def detail(request, pk):
-#     product = Products.objects.get(pk=pk)
-#     context = {
-#         'product': product
-#     }
-#     print(1)
-#     return render(request, 'detail.html', context=context)
+def detail(request, pk):
+    product = Products.objects.get(pk=pk)
+    context = {
+        'product': product
+    }
+    return render(request, 'detail.html', context=context)
+
+def add_products(request):
+    form = ProductsForm(request.POST, request.FILES)
+    if form.is_valid():
+        form.save()
+        return redirect('Products:get_info')
+    context = {
+        'form': form
+    }
+    return render(request, 'create.html', context=context)
+
+
+def update_products(request, pk):
+    data = Products.objects.get(pk=pk)
+    form = ProductsForm(request.POST, request.FILES, instance=data)
+    if form.is_valid():
+        print(1)
+        form.save()
+        return redirect('Products:get_info')
+    context = {
+        'form': form
+    }
+    return render(request, 'update.html', context=context)
+
+
+def delete_products(request, pk):
+    product = get_object_or_404(Products, pk=pk)
+    if request.method == 'POST':
+        product.delete()
+        return redirect('Products:get_info')
+
+    return render(request, 'delete.html', {'product': product})
+
+
+
